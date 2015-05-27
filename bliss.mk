59b219167ea73dbc08d9023bd6aaea53434f60a6
llama <erwanleboucher@gmail.com>
2015-05-27 16:00:50 +0200

    blissify

---
+++ bliss.mk
@@ -0,0 +1,27 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/vs980/vs980.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := vs980
+PRODUCT_NAME := bliss_vs980
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-VS980
+PRODUCT_MANUFACTURER := lge
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=lge/g2_vzw/g2:4.4.2/KOT49I.VS98024A/VS98024A.1394607468:user/release-keys \
+    PRIVATE_BUILD_DESC="g2_vzw-user 4.4.2 KOT49I.VS98024A VS98024A.1394607468 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
+
+PRODUCT_PACKAGES += Torch
